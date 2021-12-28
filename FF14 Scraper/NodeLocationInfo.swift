2eb562a48ee19e941adb7e86370510cc19f74e57
Christopher DeVito <christopher.devito@protonmail.com>
2021-12-28 11:48:43 -0500

    Added NodeLocationInfo

---
+++ FF14 Scraper/NodeLocationInfo.swift
@@ -0,0 +1,17 @@
+//
+//  NodeLocationInfo.swift
+//  FF14 Scraper
+//
+//  Created by Christopher Devito on 12/26/21.
+//
+
+import Foundation
+
+struct NodeLocationInfo {
+    let time: Int?
+    let location: String
+    let source: String
+    let stars: Int
+    let x: Double
+    let y: Double
+}
