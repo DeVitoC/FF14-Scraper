461bfc5ecfffb1375d406445c347372100b6fd97
Christopher DeVito <christopher.devito@protonmail.com>
2022-01-20 17:08:39 -0500

    Added LocationInfo Protocol and conformed NodeLocationInfo to LocationInfo

--- FF14 Scraper/NodeLocationInfo.swift
+++ FF14 Scraper/NodeLocationInfo.swift
@@ -7,11 +7,20 @@
 
 import Foundation
 
-struct NodeLocationInfo {
+struct NodeLocationInfo: LocationInfo {
     let time: Int?
     let location: String
     let source: String
     let stars: Int
     let x: Int
     let y: Int
+}
+
+protocol LocationInfo {
+    var time: Int? { get }
+    var location: String { get }
+    var source: String { get }
+    var stars: Int { get }
+    var x: Int { get }
+    var y: Int { get }
 }
