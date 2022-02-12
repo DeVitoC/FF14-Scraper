910ceeffc884fd4b9cb4ba0ac5f91c1e2f8a4ab1
Christopher DeVito <christopher.devito@protonmail.com>
2022-02-11 21:51:01 -0500

    Fixed MiningnodeScraper. Largely a rewrite of the file

--- FF14 Scraper/MiningNodeScraper.swift
+++ FF14 Scraper/MiningNodeScraper.swift
@@ -17,15 +17,15 @@ class MiningNodeScraper {
     let type = ["Submersible Components", "Bone", "Cloth", "Dye", "Ingredient", "Leather", "Lumber", "Metal", "Part", "Reagent", "Seafood", "Stone"]
     
     func scrapeGatheringNodesWiki() throws {
-        guard let consoleGamesWikiURL = URL(string: "https://ffxiv.consolegameswiki.com"), let gamerEscapeWikiURL = URL(string: "https://ffxiv.gamerescape.com") else {
-            return
-        }
+        guard let gamerEscapeWikiURL = URL(string: "https://ffxiv.gamerescape.com")
+        else { return }
+//        try scrapeGatheringNodes(gamerEscapeURL: gamerEscapeWikiURL)
         let fm = FileManager.default
         lazy var path: URL = {
             fm.urls(for: .desktopDirectory, in: .userDomainMask)[0]
         }()
         let filenames = ["MiningNodesFolklore.json", "MiningNodesUnspoiled.json", "MiningNodesNormal.json"]
-        
+
         for filename in filenames {
             let jsonDecoder = JSONDecoder()
 
@@ -37,7 +37,7 @@ class MiningNodeScraper {
                 let data = Data(jsonData)
                 let itemNames = try jsonDecoder.decode([String: String].self, from: data)
                 for (_, address) in itemNames {
-                    try scrapeGatheringNodes(consoleGamesURL: consoleGamesWikiURL, gamerEscapeURL: gamerEscapeWikiURL, testItem: address)
+                    try scrapeGatheringNodes(gamerEscapeURL: gamerEscapeWikiURL, testItem: address)
                     let seconds = 2.0
                     DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {}
                 }
@@ -45,6 +45,7 @@ class MiningNodeScraper {
                 NSLog("\(error)")
             }
         }
+
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
         let json = try encoder.encode(nodes)
@@ -59,130 +60,157 @@ class MiningNodeScraper {
         try missedNodesJsonString.write(to: missedNodesFile, atomically: true, encoding: String.Encoding.utf8)
     }
     
-    private func scrapeGatheringNodes(consoleGamesURL: URL, gamerEscapeURL: URL, testItem: String = "/wiki/Wind_Cluster") throws {
-        // scrape consoleGames Wiki
-        let consoleGamesWikiItemURL = consoleGamesURL.appendingPathComponent(testItem)
-        let consoleGamesHTML = try String(contentsOf: consoleGamesWikiItemURL)
-        let consoleGamesDocument = try SwiftSoup.parse(consoleGamesHTML)
-        
-        // get item name
-        guard let h1 = try consoleGamesDocument.select("#firstHeading").first() else {
-            print("unable to find firstHeading in consoleGameDocument")
-            missedNodes.append(testItem)
-            return
-        }
-        let itemName = try h1.text()
-        
-        // get item description
-        guard let divCG1 = try consoleGamesDocument.select("#mw-content-text").first(),
-              let divCG2 = divCG1.children().first(),
-              let divCG3 = divCG2.children().first(),
-              let blockquoteCG1 = try divCG3.nextElementSibling() else {
-                  print("unable to get divs or blockquote lines 52 - 55")
-                  missedNodes.append(testItem)
-                  return
-              }
-        var itemDescription = try blockquoteCG1.text()
-        itemDescription.removeFirst()
-        itemDescription.removeFirst()
-        itemDescription.removeSubrange(String.Index(utf16Offset: itemDescription.count - 22, in: itemDescription)...String.Index(utf16Offset: itemDescription.count - 1, in: itemDescription))
-        
-        
+    private func scrapeGatheringNodes(gamerEscapeURL: URL, testItem: String = "/wiki/Wind_Cluster") throws {
         // scrape GamerEscape wiki
         let gamerEscapeWikiItemURL = gamerEscapeURL.appendingPathComponent(testItem)
         let gamerEscapeHTML = try String(contentsOf: gamerEscapeWikiItemURL)
         let gamerEscapeDocument = try SwiftSoup.parse(gamerEscapeHTML)
         
-        // Get item elements
-        guard let divGE1 = try gamerEscapeDocument.select("#mw-content-text").first(),
-              let divGE2 = divGE1.children().first(),
-              let tableGE1 = divGE2.children().first(),
-              let tbodyGE1 = tableGE1.children().first(),
-              let trGE1 = tbodyGE1.children().first(),
-              let tdGE1 = trGE1.children().first(),
-              let tableGE2 = tdGE1.children().first(),
-              let tbodyGE2 = tableGE2.children().first(),
-              let trGE2 = tbodyGE2.children().first(),
-              let tdGE2 = trGE2.children().first(),
-              let aGE1 = tdGE2.children().first(),
-              let tdGE3 = try tdGE1.nextElementSibling(),
-              let aGE2 = tdGE3.children().first(),
-              let divGE3 = try aGE2.nextElementSibling()?.nextElementSibling(),
-              let tdGE4 = try tdGE2.nextElementSibling(),
-              let divGE4 = tdGE4.children().last(),
-              let trGE3 = try trGE1.nextElementSibling()?.nextElementSibling(),
-              let tbodyGE3 = trGE3.children().first()?.children().first()?.children().first()
+        // Get main content and the top table/info panel
+        guard let contentTextDiv = try gamerEscapeDocument.select("#mw-content-text").first(),
+              let contentParentDiv = contentTextDiv.parent(),
+              let nameH1 = contentParentDiv.children().first(),
+              let contentTextDivChild = contentTextDiv.children().first(),
+              let topInfoTable = contentTextDivChild.children().first(),
+              let topInfoTbody = topInfoTable.children().first()
         else {
-            print("unable to get element in lines 71 - 88")
+            print("unable to get element in lines 69 - 74: main content and top info panel")
             missedNodes.append(testItem)
             return
         }
-        
-        // assign item values
-        let itemImgUrl = try aGE1.attr("href")
-        let patchText = try divGE3.text().components(separatedBy: " ")[1]
-        guard let patchNumber = Float(patchText) else { return }
+
+        // MARK: Get Name
+        let itemName = try nameH1.text()
+
+        // Get name bar left elements
+        guard let nameBarTR = topInfoTbody.children().first(),
+              let nameBarLeftTD = nameBarTR.children().first(),
+              let nameBarLeftTable = nameBarLeftTD.children().first(),
+              let nameBarLeftTbody = nameBarLeftTable.children().first(),
+              let nameBarLeftTR = nameBarLeftTbody.children().first(),
+              let imageTD = nameBarLeftTR.children().first(),
+              let imageA = imageTD.children().first(),
+              let nameAndTypeTD = try imageTD.nextElementSibling(),
+              let typeDiv = nameAndTypeTD.children().last()
+        else {
+            print("unable to get elements in lines 85 - 93")
+            missedNodes.append(testItem)
+            return
+        }
+
+        // MARK: Get Image URL
+        let itemImgUrl = try imageA.attr("href")
+
+        // MARK: Get Type
+        let itemType = try typeDiv.text()
+
+        // Get name bar right elements
+        guard let expacTD = try nameBarLeftTD.nextElementSibling(),
+              let itemA = expacTD.children().first(),
+              let expacNumberDiv = try itemA.nextElementSibling()?.nextElementSibling()
+        else {
+            print("unable to get elements in lines 107 - 109")
+            missedNodes.append(testItem)
+            return
+        }
+
+        // TODO: Fix Expac
+        // MARK: Get Expac
+        var patchText = try expacNumberDiv.text().components(separatedBy: " ")[1]
+        patchText = patchText.replacingOccurrences(of: "a", with: "").replacingOccurrences(of: "b", with: "")
+        guard let patchNumber = Float(patchText)
+        else {
+            print("Failed to convert patch number on line 118")
+            missedNodes.append(testItem)
+            return
+        }
         let itemExpac = Int(patchNumber - 2)
-        let itemType = try divGE4.text()
+
+        // Get Info Panel Bottom
+        guard let infoPanelBottomTR = try nameBarTR.nextElementSibling()?.nextElementSibling(),
+              let infoPanelBottomTBody = infoPanelBottomTR.children().first()?.children().first()?.children().first()
+        else {
+            print("unable to get element in lines 127 - 128")
+            missedNodes.append(testItem)
+            return
+        }
+
+        // MARK: Get Description
+        let descriptionHeaderTR = try infoPanelBottomTBody.children().filter({ try $0.text().contains("Description:") })
+        guard let descriptionTR = try descriptionHeaderTR.first?.nextElementSibling()
+        else {
+            print("unable to get elements in line 137")
+            missedNodes.append(testItem)
+            return
+        }
+        let itemDescription = try descriptionTR.text()
+
+        // MARK: Get Item lvl
         var itemLvl: Int = 0
-        for tr in tbodyGE3.children() {
+        for tr in infoPanelBottomTBody.children() {
             guard let td1 = tr.children().first(),
                   try td1.text().contains("Item Level"),
                   let td2 = try td1.nextElementSibling(),
                   let td2Text = Int(try td2.text())
             else {
+                print("unable to get items in lines 151 - 154")
                 continue
             }
             itemLvl = td2Text
         }
-        
+
         // get gathering location elements
         var itemLocationInfo: [NodeLocationInfo] = []
         for gatheringSource in ["Quarrying", "Mining"] {
-            guard let spanGE1 = try gamerEscapeDocument.select("#\(gatheringSource)").first() else { continue }
-            guard let divGE5 = spanGE1.parent(),
-                  let thGE1 = divGE5.parent(),
-                  let trGE4 = thGE1.parent(),
-                  let tableGE3 = try trGE4.nextElementSibling()?.children().first()?.children().first(),
-                  let tbodyGE4 = tableGE3.children().first()
+            // Get section for specific gathering source
+            guard let gatheringSourceSpan = try gamerEscapeDocument.select("#\(gatheringSource)").first()
             else {
-                print("unable to get element in lines 119 - 123")
+                print("line 162: \(itemName)")
+                continue
+            }
+
+            // MARK: Get Gathering Source
+            let itemSource = gatheringSource
+
+            // Get Element Table element for gathering Source
+            guard let gatheringSourceParentDiv = gatheringSourceSpan.parent(),
+                  let gatheringSourceParentTH = gatheringSourceParentDiv.parent(),
+                  let gatheringSourceParentTR = gatheringSourceParentTH.parent(),
+                  let gatheringSourceTable = try gatheringSourceParentTR.nextElementSibling()?.children().first()?.children().first(),
+                  let gatheringSourceTBody = gatheringSourceTable.children().first()
+            else {
+                print("unable to get element in lines 172 - 176")
                 missedNodes.append(testItem)
                 return
             }
             
             // get location information
-            for (index, tr) in tbodyGE4.children().enumerated() {
-                guard index != 0 else {
-                    continue
-                }
-                guard let thisTD1 = tr.children().first(), // to get location and coords and source
-                      let thisTD2 = try thisTD1.nextElementSibling(), // to get time
-                      let thisTD3 = try thisTD2.nextElementSibling(), // to get stars
-                      let thisSmall = thisTD1.children().first()
+            for tr in gatheringSourceTBody.children() {
+                guard let nodeSlotTD = tr.children().first(), // to get location and coords
+                      let nodeReqTD = try nodeSlotTD.nextElementSibling(), // to get time
+                      let gatheringReqTD = try nodeReqTD.nextElementSibling(), // to get stars
+                      let nodeSlotSmall = nodeSlotTD.children().first()
                 else {
-                    print("unable to get element in lines 135 - 138")
+                    print("unable to get element in lines 185 - 188")
                     continue
                 }
-                
-                // get source and location for this node
-                var itemSource = ""
+
+                // MARK: Get Location
                 var itemLocation = ""
-                for child in thisSmall.children() {
+                for child in nodeSlotSmall.children() {
                     if child.tagName() == "br" {
                         continue
                     }
                     let childText = try child.text()
                     if childText.contains(":") {
-                        let childTextElements = childText.components(separatedBy: ":")
-                        itemSource = childTextElements[0]
+                        continue
                     } else {
                         itemLocation = childText
                     }
                 }
-                
-                // get coords
-                let thisSmallText = try thisSmall.text()
+
+                // MARK: Get Coords
+                let thisSmallText = try nodeSlotSmall.text()
                 var x: Int = 0, y: Int = 0
                 if let openParenIndex = thisSmallText.lastIndex(of: "("),
                    let closeParenIndex = thisSmallText.lastIndex(of: ")") {
@@ -197,43 +225,55 @@ class MiningNodeScraper {
                         y = Int(Float(coords[1]) ?? 0)
                     }
                 } else {
-                    print("unable to get elements in lines 163 - 164")
+                    print("unable to get elements in lines 212 - 213")
+                    missedNodes.append(testItem)
+                    return
                 }
                 
                 // get Time
-                let thisTD2Text = try thisTD2.text()
-                var time = 0
+                let thisTD2Text = try nodeReqTD.text()
+                var time: Int? = nil
                 var timeOfDay = ""
                 var itemTimes: [Int?] = []
-                for thisTime in thisTD2Text.components(separatedBy: "/") {
-                    for component in thisTime.components(separatedBy: " ") {
-                        if let timeInt = Int(component) {
-                            time = timeInt
-                        } else if component == "AM" || component == "PM" {
-                            timeOfDay = component
+                if thisTD2Text.contains("AM") || thisTD2Text.contains("PM") {
+                    for thisTime in thisTD2Text.components(separatedBy: "/") {
+                        for component in thisTime.components(separatedBy: " ") {
+                            if let timeInt = Int(component) {
+                                time = timeInt
+                            } else if component == "AM" || component == "PM" {
+                                timeOfDay = component
+                            }
                         }
-                    }
-                    if timeOfDay == "AM" {
-                        itemTimes.append(time)
-                    } else if timeOfDay == "PM" {
-                        itemTimes.append(time + 12)
+                        if timeOfDay == "AM" {
+                            itemTimes.append(time)
+                        } else if timeOfDay == "PM" {
+                            if let time = time {
+                                itemTimes.append(time + 12)
+                            }
+                        }
                     }
                 }
 
                 // get stars
-                let thisTD3Elements = try thisTD3.text().components(separatedBy: " Perception ")
+                let thisTD3Elements = try gatheringReqTD.text().components(separatedBy: " Perception ")
                 let itemStars = thisTD3Elements[0].components(separatedBy: " ").last?.count ?? 0
-                
-                for time in itemTimes {
+
+                if itemTimes.isEmpty {
                     let locationInfo = NodeLocationInfo(time: time, location: itemLocation, source: itemSource, stars: itemStars, x: x, y: y)
                     itemLocationInfo.append(locationInfo)
+                } else {
+                    for time in itemTimes {
+                        let locationInfo = NodeLocationInfo(time: time, location: itemLocation, source: itemSource, stars: itemStars, x: x, y: y)
+                        itemLocationInfo.append(locationInfo)
+                    }
                 }
             }
         }
 
         for itemLocation in itemLocationInfo {
             let gatheringItem = GatheringNode(name: itemName, time: itemLocation.time, location: itemLocation.location, img: itemImgUrl, description: itemDescription, type: itemType, source: itemLocation.source, lvl: itemLvl, stars: itemLocation.stars, x: itemLocation.x, y: itemLocation.y, expac: itemExpac, gathering: 0)
-                nodes.append(gatheringItem)
+            print(gatheringItem)
+            nodes.append(gatheringItem)
         }
     }
 }
