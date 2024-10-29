48bfc57680bddfaf5c306a18f73b9af13e6596ad
Christopher DeVito <christopher.devito@protonmail.com>
2024-10-29 00:16:25 -0400

    organize scrapers in main.swift

--- FF14 Scraper/main.swift
+++ FF14 Scraper/main.swift
@@ -9,45 +9,52 @@ import Foundation
 import SwiftSoup
 
 // MARK: - Scraper Objects
-let botanyNodesScraper = BotanyNodesScraper()
+// Botany Scrapers
+//let botanyNodesScraper = BotanyNodesScraper() // updated 10-28-2024
+//let botanyNodeScraper = BotanyNodeScraper() // updated 10-28-2024
 let botanyNodesFolkloreScraper = BotanyNodeFolkloreScraper()
-let botanyNodesUnspoiledScraper = BotanyNodeUnspoiledScraper()
-let botanynodesEphermeralScraper = EphemeralNodesScraper()
-let miningNodesFolkloreScraper = MiningNodeFolkloreScraper()
-let fishingNodesFolkloreScraper = FishingNodeFolkloreScraper()
-let miningNodeUnspoiledScraper = MiningNodeUnspoiledScraper()
-let miningNodesScraper = MiningNodesScraper()
-let fishingNodesScraper = FishingNodesScraper()
-let fishingCollectiblesScraper = FishingCollectiblesScraper()
-let botanyNodeScraper = BotanyNodeScraper()
-let miningNodeScraper = MiningNodeScraper()
-let fishingNodeScraper = FishingNodeScraper()
-let fetchImagesScraper = FetchImagesScraper()
-let updateExpac = UpdateExpac()
-let updateName = UpdateName()
-let updateFishingDescript = UpdateFishingDescription()
-let removeDuplicates = RemoveDuplicates()
-let addWeatherChain = AddWeatherChain()
+//let botanyNodesUnspoiledScraper = BotanyNodeUnspoiledScraper()
+//let botanynodesEphermeralScraper = EphemeralNodesScraper()
+
+// Mining Scrapers
+//let miningNodesScraper = MiningNodesScraper()
+//let miningNodeScraper = MiningNodeScraper()
+//let miningNodesFolkloreScraper = MiningNodeFolkloreScraper()
+//let miningNodeUnspoiledScraper = MiningNodeUnspoiledScraper()
+
+// Fishing Scrapers
+//let fishingNodesScraper = FishingNodesScraper()
+//let fishingNodesFolkloreScraper = FishingNodeFolkloreScraper()
+//let fishingCollectiblesScraper = FishingCollectiblesScraper()
+//let fishingNodeScraper = FishingNodeScraper()
+//let addWeatherChain = AddWeatherChain()
+
+// Misc scrapers
+//let removeDuplicates = RemoveDuplicates()
+//let fetchImagesScraper = FetchImagesScraper()
+//let updateExpac = UpdateExpac()
+//let updateName = UpdateName()
+//let updateFishingDescript = UpdateFishingDescription()
 
 
 // MARK: - Scraper calls
 // Uncomment call to run that scraper
-try botanyNodesScraper.scrapeBotanyNodesWiki()
+//try botanyNodesScraper.scrapeBotanyNodesWiki()
+//try botanyNodeScraper.scrapeGatheringNodesWiki()
 try botanyNodesFolkloreScraper.scrapeBotanyNodesWiki()
-try botanyNodesUnspoiledScraper.scrapeBotanyNodesWiki()
-try botanynodesEphermeralScraper.scrapeBotanyNodesWiki()
+//try botanyNodesUnspoiledScraper.scrapeBotanyNodesWiki()
+//try botanynodesEphermeralScraper.scrapeBotanyNodesWiki()
 //try miningNodesFolkloreScraper.scrapeMiningNodesWiki()
 //try fishingNodesFolkloreScraper.scrapeFishingNodesWiki()
 //try miningNodeUnspoiledScraper.scrapeMiningNodesWiki()
 //try miningNodesScraper.scrapeMiningNodesWiki()
 //try fishingNodesScraper.scrapeFishingNodesWiki()
 //try fishingCollectiblesScraper.scrapeFishingCollectiblesWiki()
-try botanyNodeScraper.scrapeGatheringNodesWiki()
 //try miningNodeScraper.scrapeGatheringNodesWiki()
 //try fishingNodeScraper.scrapeGatheringNodesWiki()
-fetchImagesScraper.fetchFromJSONFile()
-try updateExpac.fixExpacData()
+//fetchImagesScraper.fetchFromJSONFile()
+//try updateExpac.fixExpacData()
 //try updateName.fixExpacData()
 //try updateFishingDescript.fixFishDesc()
-try removeDuplicates.fixDuplicates()
+//try removeDuplicates.fixDuplicates()
 //try addWeatherChain.fixWeather()
