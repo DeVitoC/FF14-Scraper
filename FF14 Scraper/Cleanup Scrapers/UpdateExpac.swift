3fa9e77998edeb321a9349df9ab3580fe1746b81
Christopher DeVito <christopher.devito@protonmail.com>
2024-11-23 18:55:10 -0500

    Update UpdateExpac.swift

--- FF14 Scraper/Cleanup Scrapers/UpdateExpac.swift
+++ FF14 Scraper/Cleanup Scrapers/UpdateExpac.swift
@@ -8,42 +8,67 @@
 import Foundation
 
 class UpdateExpac {
-    private var finalNodes: [GatheringNode] = []
-    private var nodesToModify: [GatheringNode] = []
     private var listToCheck: [String] = []
     private let fm = FileManager.default
     private lazy var path: URL = {
         let path = fm.urls(for: .desktopDirectory, in: .userDomainMask)[0]
         return path
     }()
-    private let fileName = "AllBotanyNodes.json"
+    private let fileNames = ["Botany", "Mining", "Fishing"]
+    let decoder = JSONDecoder()
 
     func fixExpacData() throws {
-        getNodes()
-        fixExpacNumber()
-        try encodeAndSave()
+        for file in fileNames {
+            print("file: ", file)
+            let nodesToUpdate: [Node]
+
+            if file == "Fishing" {
+                nodesToUpdate = getFishingNodes(fileName: file)
+            } else {
+                nodesToUpdate = getNodes(fileName: file)
+            }
+
+            print("nodesToUpdate: ", nodesToUpdate)
+            let updatedNodes = fixExpacNumber(nodesToModify: nodesToUpdate)
+            try encodeAndSave(finalNodes: updatedNodes, fileName: file)
+        }
     }
 
-    private func getNodes() {
-        let decoder = JSONDecoder()
-
-        // Get JSON data
-        guard let jsonData = NSData(contentsOfFile: path.appendingPathComponent(fileName).path)
-        else {
-            print("Failed to get JSON data from file")
-            return
-        }
+    private func getNodes(fileName: String) -> [GatheringNode] {
+        let data = loadFileData(fileName: fileName)
         // Decode JSON
         do {
-            let data = Data(jsonData)
-            nodesToModify = try decoder.decode([GatheringNode].self, from: data)
-            print(nodesToModify.count)
+            let nodes = try decoder.decode([GatheringNode].self, from: data)
+            return nodes
         } catch let error {
             NSLog("\(error)")
+            return []
         }
     }
 
-    private func fixExpacNumber() {
+    private func getFishingNodes(fileName: String) -> [FishingNode] {
+        let data = loadFileData(fileName: fileName)
+        // Decode JSON
+        do {
+            let nodes = try decoder.decode([FishingNode].self, from: data)
+            return nodes
+        } catch let error {
+            NSLog("\(error)")
+            return []
+        }
+    }
+
+    private func loadFileData(fileName: String) -> Data {
+        guard let jsonData = NSData(contentsOfFile: path.appendingPathComponent("All\(fileName)Nodes.json").path)
+        else {
+            print("Failed to get JSON data from file")
+            return Data()
+        }
+        return Data(jsonData)
+    }
+
+    private func fixExpacNumber(nodesToModify: [Node]) -> [Node] {
+        var finalNodes: [Node] = []
         for node in nodesToModify {
             var newNode = node
             switch node.location {
@@ -57,25 +82,33 @@ class UpdateExpac {
                 newNode.expac = 3
             case "Old Sharlayan", "Radz-at-Han", "Labyrinthos", "Thavnair", "Garlemald", "Mare Lamentorum", "Elpis", "Ultima Thule", "Empyreum":
                 newNode.expac = 4
+            case "Tuliyollal", "Urqopacha", "Yak T'el", "Kozama'uka", "Shaaloani", "Heritage Found", "Living Memory":
+                newNode.expac = 5
             default:
                 listToCheck.append(newNode.name)
             }
             finalNodes.append(newNode)
         }
+        return finalNodes
     }
 
-    private func encodeAndSave() throws {
+    private func encodeAndSave(finalNodes: [Node], fileName: String) throws {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
-        let json = try encoder.encode(finalNodes)
+        var json: Data
+        if fileName == "Fishing" {
+            json = try encoder.encode(finalNodes as? [FishingNode] ?? [])
+        } else {
+            json = try encoder.encode(finalNodes as? [GatheringNode] ?? [])
+        }
         let jsonString = String(decoding: json, as: UTF8.self)
 
-        let outputFile = URL(fileURLWithPath: "/Users/christopherdevito/Desktop/AllBotanyNodes.json")
+        let outputFile = URL(fileURLWithPath: "/Users/christopherdevito/Desktop/All\(fileName)Nodes.json")
         try jsonString.write(to: outputFile, atomically: true, encoding: String.Encoding.utf8)
 
         let missedNodesJson = try encoder.encode(listToCheck)
         let missedNodesJsonString = String(decoding: missedNodesJson, as: UTF8.self)
-        let missedNodesFile = URL(fileURLWithPath: "/Users/christopherdevito/Desktop/BotanyNodesToCheckExpac.json")
+        let missedNodesFile = URL(fileURLWithPath: "/Users/christopherdevito/Desktop/\(fileName)NodesToCheckExpac.json")
         try missedNodesJsonString.write(to: missedNodesFile, atomically: true, encoding: String.Encoding.utf8)
     }
 }
