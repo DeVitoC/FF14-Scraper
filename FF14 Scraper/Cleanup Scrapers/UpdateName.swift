2cb0560e41b47f1b31420ec7e9bb9b2fed6c17bc
Christopher DeVito <christopher.devito@protonmail.com>
2024-11-23 20:23:21 -0500

    Update UpdateName

--- FF14 Scraper/Cleanup Scrapers/UpdateName.swift
+++ FF14 Scraper/Cleanup Scrapers/UpdateName.swift
@@ -8,61 +8,71 @@
 import Foundation
 
 class UpdateName {
-    private var finalNodes: [FishingNode] = []
-    private var nodesToModify: [FishingNode] = []
-    private var listToCheck: [String] = []
     private let fm = FileManager.default
     private lazy var path: URL = {
         let path = fm.urls(for: .desktopDirectory, in: .userDomainMask)[0]
         return path
     }()
-    private let fileName = "AllFishingNodes.json"
+    private let fileNames = ["Botany", "Mining", "Fishing"]
 
-    func fixExpacData() throws {
-        getNodes()
-        fixName()
-        try encodeAndSave()
+    func fixNames() throws {
+        for fileName in fileNames {
+            let nodesToModify = getNodes(fileName: fileName)
+            let finalNodes = fixName(nodesToModify: nodesToModify)
+            try encodeAndSave(finalNodes: finalNodes, fileName: fileName)
+        }
     }
 
-    private func getNodes() {
+    private func getNodes(fileName: String) -> [Node] {
         let decoder = JSONDecoder()
 
         // Get JSON data
-        guard let jsonData = NSData(contentsOfFile: path.appendingPathComponent(fileName).path)
+        guard let jsonData = NSData(contentsOfFile: path.appendingPathComponent("All\(fileName)Nodes.json").path)
         else {
-            print("Failed to get JSON data from file")
-            return
+            print("Failed to get JSON data from file, ", fileName)
+            return []
         }
         print("got data")
         // Decode JSON
         do {
             let data = Data(jsonData)
-            nodesToModify = try decoder.decode([FishingNode].self, from: data)
+            let nodesToModify: [Node]
+            if fileName == fileNames[2] {
+                nodesToModify = try decoder.decode([FishingNode].self, from: data)
+            } else {
+                nodesToModify = try decoder.decode([GatheringNode].self, from: data)
+            }
+            return nodesToModify
         } catch let error {
             NSLog("\(error)")
+            return []
         }
     }
 
-    private func fixName() {
+    private func fixName(nodesToModify: [Node]) -> [Node] {
+        var finalNodes: [Node] = []
         for node in nodesToModify {
             var newNode = node
             newNode.name = node.name.replacingOccurrences(of: "_", with: " ")
             finalNodes.append(newNode)
         }
+
+        return finalNodes
     }
 
-    private func encodeAndSave() throws {
+    private func encodeAndSave(finalNodes: [Node], fileName: String) throws {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
-        let json = try encoder.encode(finalNodes)
+
+        let json: Data
+        if fileName == fileNames[2] {
+            json = try encoder.encode(finalNodes as! [FishingNode])
+        } else {
+            json = try encoder.encode(finalNodes as! [GatheringNode])
+        }
         let jsonString = String(decoding: json, as: UTF8.self)
 
-        let outputFile = URL(fileURLWithPath: "/Users/christopherdevito/Desktop/AllFishingNodes.json")
+        let outputFile = URL(fileURLWithPath: "/Users/christopherdevito/Desktop/All\(fileName)Nodes.json")
         try jsonString.write(to: outputFile, atomically: true, encoding: String.Encoding.utf8)
-
-        let missedNodesJson = try encoder.encode(listToCheck)
-        let missedNodesJsonString = String(decoding: missedNodesJson, as: UTF8.self)
-        let missedNodesFile = URL(fileURLWithPath: "/Users/christopherdevito/Desktop/FishingNodesToCheck.json")
-        try missedNodesJsonString.write(to: missedNodesFile, atomically: true, encoding: String.Encoding.utf8)
     }
 }
